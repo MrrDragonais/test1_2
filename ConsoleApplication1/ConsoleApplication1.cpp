19ceb8372215633dcc92b295e32df1dc18f7369a
nikit <nikit@DESKTOP-5363RTP>
2023-04-11 19:03:47 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+
+class Road
+{
+public:
+	double length;
+	int width;
+	Road();
+};
+
+Road::Road()
+{
+	length = 0;
+	width = 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "TeTest v1.2: Ok.\n";
+
+	Road road;
+
+	cout << "Введите длину дороги: " << endl;
+	cin >> road.length;
+	cout << "Введите ширину дороги: " << endl;
+	cin >> road.width;
+
+	cout << " Length: " << road.length << endl;
+	cout << "Width: " << road.width << endl;
+	return 0;
+}
